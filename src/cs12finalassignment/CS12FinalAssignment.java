2aa5327c6e3ef245922c82230d88acaa493ec2c1
jjb <jjb@DESKTOP-N83EEJJ>
2026-06-01 20:45:26 -0400

    First message for CS12FinalAssignment

---
+++ src/cs12finalassignment/CS12FinalAssignment.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cs12finalassignment;
+
+/**
+ *
+ * @author jjb
+ */
+public class CS12FinalAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
